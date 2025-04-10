e5476fc5a694f4392940f4aad1b11d9434c9f3ff
Damiriko <damiriko80@gmail.com>
2025-04-10 22:34:20 +0300

    fields static

--- src/Field/Field.js
+++ src/Field/Field.js
@@ -1,8 +1,30 @@
+import styles from './Field.module.css'
 
 const FieldLayout = () => {
 	return (
 		<>
-			<label>this Field </label>
+
+			<div className={styles.field}>
+				<button className={styles.cell}></button>
+				<button className={styles.cell}></button>
+				<button className={styles.cell}></button>
+				<button className={styles.cell}></button>
+				<button className={styles.cell}></button>
+				<button className={styles.cell}></button>
+				<button className={styles.cell}></button>
+				<button className={styles.cell}></button>
+				<button className={styles.cell}></button>
+			</div>
+			<img
+				src="/sprites/Knight1.jpg"
+				alt="Рыцарь"
+				className={styles.knight}
+			/>
+			<img
+				src="/sprites/dragon-not_Fire.png"
+				alt="Дракон"
+				className={styles.dragon}
+			/>
 		</>
 	);
 }
