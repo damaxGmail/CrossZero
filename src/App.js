79649a5ec164b83b74881bfae8b6a8c314cb67cc
Damiriko <damiriko80@gmail.com>
2025-05-01 01:07:32 +0300

    empty Hook Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { ScreenSaver } from './ScreenSaver/ScreenSaver';
 import { Registration } from './Registration/Registration';
+import { Registration2 } from './Registration2/Registration2';
 import { Game } from './Game/Game';
 
 
 const AppLayout = ({
 	page,
 	goToRegistration,
+	goToRegistration2,
 	goToGame,
 
 }) => {
@@ -16,7 +18,8 @@ const AppLayout = ({
 		<>
 			<div>
 				{page === 'screensaver' && <ScreenSaver onStart={goToRegistration} />}
-				{page === 'registration' && <Registration onStartGame={goToGame} />}
+				{page === 'registration' && <Registration onStartGame={goToRegistration2} />}
+				{page === 'registration2' && <Registration2 onStartGame={goToGame} />}
 				{page === 'game' && <Game />}
 			</div>
 		</>
@@ -24,14 +27,17 @@ const AppLayout = ({
 }
 
 export const App = () => {
-	const [page, setPage] = useState('screensaver'); // Текущая страница
-	//const [page, setPage] = useState('registration'); // временно
+	//const [page, setPage] = useState('screensaver'); // Текущая страница
+	const [page, setPage] = useState('registration2'); // временно
 
 	// Функция для перехода на страницу регистрации
 	const goToRegistration = () => {
 		setPage('registration');
 	};
 
+	const goToRegistration2 = () => {
+		setPage('registration2');
+	};
 	// Функция для перехода на страницу игры
 	const goToGame = () => {
 		setPage('game');
@@ -40,6 +46,7 @@ export const App = () => {
 	return <AppLayout
 		page={page}
 		goToRegistration={goToRegistration}
+		goToRegistration2={goToRegistration2}
 		goToGame={goToGame}
 	/>;
 
---
+++ src/Registration2/Registration2.js
@@ -0,0 +1,220 @@
+import React, { useState, useEffect } from 'react';
+import styles from './Registration2.module.css';
+import { playSound } from '../Effect/Effect';
+
+const sendFormData = (formData) => {
+
+	console.log(formData);
+};
+
+
+const RegistrationLayout = ({ onStartGame, stopMusic }) => {
+	const [login, setLogin] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [doublePassword, setDoublePassword] = useState('');
+
+	const [loginError, setLoginError] = useState(null);
+
+	const onLoginChange = ({ target }) => {
+		setLogin(target.value);
+
+		let newError = null;
+
+		if (!/^[\w_]*$/.test(target.value)) {
+			newError = 'Неверный логин. Допустимые символы: буквы, цифры и нижнее подчёркивание';
+		} else if (target.value.length > 20) {
+			newError = 'Неверный логин. Должно быть не больше 20 символов';
+		}
+
+		setLoginError(newError);
+	};
+
+	const validateDoublePassword = (password, doublePassword) => {
+
+		if (password !== doublePassword) {
+			return 'Введенные пароли не совпадают';
+		}
+
+		return null;
+	}
+	const onDoublePasswordChange = ({ target }) => {
+		const newDoublePassword = target.value;
+		setDoublePassword(newDoublePassword);
+
+		const errors = validateDoublePassword(password, newDoublePassword);
+
+		setLoginError(errors);
+
+	};
+
+	const validatePassword = (password, login) => {
+		const errors = [];
+
+		if (password.length < 4 || password.length > 64) {
+			errors.push('Пароль должен быть длиной от 4 до 64 символов.');
+		}
+
+		if (/\s/.test(password)) {
+			errors.push('Пароль не должен содержать пробелы.');
+		}
+
+		if (!password || !login) return ['Некорректные данные для валидации.'];
+		if (
+			password.toLowerCase().includes(login.toLowerCase())
+		) {
+			errors.push('Пароль не должен быть похож на логин .');
+		}
+
+		return errors;
+	};
+
+	const onPasswordChange = ({ target }) => {
+		const newPassword = target.value;
+		setPassword(newPassword);
+
+
+		// Выполняем валидацию пароля
+		const errors = validatePassword(newPassword, login);
+		if (errors.length > 0) {
+			setLoginError(errors[0]);
+		} else {
+			setLoginError(null);
+		}
+	};
+
+	const onLoginBlur = ({ target }) => {
+		if (target.value.length < 3) {
+			setLoginError('Неверный логин. Должно быть не меньше 3 символов');
+		}
+	};
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		sendFormData({ login, email, password });
+	};
+
+
+	const handleRegister = () => {
+		sendFormData({ login, email, password });
+
+		stopMusic();
+		onStartGame(); // Переход к игре
+	};
+
+	const onEmailChange = ({ target }) => {
+		setEmail(target.value);
+		let newError = null;
+
+		if (!target.value.includes('@')) {
+			newError = 'Неверный email. Должен содержать символ "@"';
+		}
+		else if (target.value.length < 5) {
+			newError = 'Неверный email. Слишком короткий адрес';
+		}
+
+		setLoginError(newError);
+	}
+
+
+	return (
+		<>
+			<div className={styles.registrationContainer}>
+				<div className={styles.fildRegistration}>
+
+					<h1> 2) Регистрация Hook Form</h1>
+					<form className={styles.form} onSubmit={onSubmit}>
+						{/* Поле Login */}
+						<div className={styles.inputGroup}>
+							{!loginError && <label>Login:</label>}
+							{loginError && <div className={styles.error}>{loginError}</div>}
+
+							<input
+								name="login"
+								type="text"
+								value={login}
+								placeholder="Введите Ваш Логин"
+								className={styles.input}
+								onChange={onLoginChange}
+								onBlur={onLoginBlur}
+							/>
+						</div>
+						{/* Поле Email */}
+						<div className={styles.inputGroup}>
+							{!loginError && <label>Email:</label>}
+							{loginError && <div className={styles.error}>{loginError}</div>}
+							<input
+								type="email"
+								value={email}
+								placeholder="Введите email"
+								className={styles.input}
+								onChange={onEmailChange}
+							/>
+						</div>
+
+						{/* Поле Пароль */}
+						<div className={styles.inputGroup}>
+							{!loginError && <label>Пароль:</label>}
+							{loginError && <div className={styles.error}>{loginError}</div>}
+							<input
+								type="password"
+								value={password}
+								placeholder="Введите пароль"
+								className={styles.input}
+								onChange={onPasswordChange}
+							/>
+						</div>
+
+						{/* Поле Подтверждение пароля */}
+						<div className={styles.inputGroup}>
+							{!loginError && <label>Подтвердите пароль:</label>}
+							{loginError && <div className={styles.error}>{loginError}</div>}
+							<input
+								type="password"
+								value={doublePassword}
+								placeholder="Подтвердите пароль"
+								className={styles.input}
+								onChange={onDoublePasswordChange}
+							/>
+						</div>
+
+						<button type="submit" disabled={!!loginError} className={styles.buttonReg} onClick={handleRegister}>Зарегистрироваться</button>
+					</form>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export const Registration2 = ({ onStartGame }) => {
+	const [audio, setAudio] = useState(null);
+
+
+	//временно
+	// useEffect(() => {
+
+	// 	const currentAudio = playSound(null, 'Registration');
+	// 	setAudio(currentAudio);
+
+	// 	return () => {
+	// 		if (currentAudio) {
+	// 			currentAudio.pause();
+	// 			currentAudio.currentTime = 0;
+	// 		}
+	// 	};
+
+
+	// }, []);
+
+	const stopMusic = () => {
+		if (audio) {
+			audio.pause(); // Останавливаем воспроизведение
+			audio.currentTime = 0; // Сбрасываем позицию звука
+		}
+	};
+
+	return <RegistrationLayout
+		onStartGame={onStartGame}
+		stopMusic={stopMusic}
+	/>;
+}
